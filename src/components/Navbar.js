d6e0c5bade44997f60347a7f5e8c5a8a60a44d82
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-09 20:46:45 +0530

    Create Component for Navbar, Hero, About, Servces, Tours and Footer

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import logo from '../images/logo.svg';
+import { pageLinks } from '../data';
+import { socialLinks } from '../data';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map(({ id, href, text }) => {
+            return (
+              <li key={id}>
+                <a href={href} className="nav-link">
+                  {text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialLinks.map(({ id, href, icon }) => {
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  className="nav-icon"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
